8386dfa3058b2085519c8cbf71fb0be30fde626a
Javlon <59303287+javlona@users.noreply.github.com>
2024-03-07 18:57:01 -0600

    useMediaQuery added

---
+++ src/useMediaQuery/useMediaQuery.js
@@ -0,0 +1,33 @@
+import { useState, useEffect, useRef } from "react";
+
+export default function useMediaQuery(mediaQuery) {
+  const [isMatch, setIsMatch] = useState(false);
+  const [mediaQueryList, setMediaQueryList] = useState(null);
+
+  useEffect(() => {
+    const list = window.matchMedia(mediaQuery);
+    setMediaQueryList(list);
+    setIsMatch(list.matches);
+  }, [mediaQuery]);
+
+  useEventListener("change", (e) => setIsMatch(e.matches), mediaQueryList);
+
+  return isMatch;
+}
+
+// useEventLister begin
+function useEventListener(eventType, callback, element = window) {
+  const callbackRef = useRef(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (element === null) return;
+    const handler = (e) => callbackRef.current(e);
+    element.addEventListener(eventType, handler);
+
+    return () => element.removeEventListener(eventType, handler);
+  }, [eventType, element]);
+}
