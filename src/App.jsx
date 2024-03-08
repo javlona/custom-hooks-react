f2fb12e432b8ac5ae2e5466c8b9f724aab2a86a9
Javlon <59303287+javlona@users.noreply.github.com>
2024-03-07 18:31:07 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Custom Hooks</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/UseStorage/useStorage.js
@@ -0,0 +1,33 @@
+import { useState, useEffect, useCallback } from "react";
+
+export function useLocalStorage(key, defaultValue) {
+  return useStorage(key, defaultValue, window.localStorage);
+}
+
+export function useSessionStorage(key, defaultValue) {
+  return useStorage(key, defaultValue, window.sessionStorage);
+}
+
+function useStorage(key, defaultValue, storageObject) {
+  const [value, setValue] = useState(() => {
+    const jsonValue = storageObject.getItem(key);
+    if (jsonValue != null) return JSON.parse(jsonValue);
+
+    if (typeof defaultValue === "function") {
+      return defaultValue();
+    } else {
+      return defaultValue;
+    }
+  });
+
+  useEffect(() => {
+    if (value === undefined) return storageObject.remove(key);
+    storageObject.setItem(key, JSON.stringify(value));
+  }, [key, value, storageObject]);
+
+  const remove = useCallback(() => {
+    setValue(undefined);
+  }, []);
+
+  return [value, setValue, remove];
+}
